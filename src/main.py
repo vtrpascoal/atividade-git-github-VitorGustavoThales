30fb87733d7b210a0fb055764e2e8b008b7bf94b
vitorpascoal <vitorlimapp@gmail.com>
2026-05-19 00:31:31 -0300

    criar while e condicional

---
+++ src/main.py
@@ -0,0 +1,9 @@
+while True:
+    print("1 - Cadastrar aluno")
+    print("2 - Sair")
+    escolha = int(input("Digite uma opção"))
+
+    if escolha == 1:
+        input("Digite o nome do aluno: ")
+        input("Digite a turma do aluno:")
+        
